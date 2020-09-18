4f01ddaf0f63e56d60320b8ebf522c092f103f72
Viacheslav <vvolos@softserveinc.com>
2020-09-18 11:03:57 +0300

    init

---
+++ src/App.types.ts
@@ -0,0 +1,86 @@
+export interface User {
+    username: string;
+    password: string;
+}
+
+export interface AppProps extends User {
+    snowInstallInstance: string;
+}
+
+export interface ErrorResult {
+    status: string;
+    status_label: string;
+    status_message: string;
+    status_detail: string;
+    error: string;
+}
+
+export enum Errors {
+    USERNAME = 'snowUsername is not set',
+    PASSWORD = 'snowPassword is not set',
+    INSTALL_INSTANCE = 'snowInstallInstance is not set',
+    SUITE_SYS_ID_OR_NAME = 'Set testSuiteSysId or testSuiteName please',
+    INCORRECT_CONFIG = 'Configuration is incorrect',
+    CANCELLED = 'Canceled',
+    TESTS_FAILED = 'Testsuite run failed',
+    TEST_SUITE_FAILED = 'Testsuite failed',
+}
+
+export interface RequestOptions {
+    test_suite_sys_id?: string | undefined;
+    test_suite_name?: string | undefined;
+    browser_name?: string | undefined;
+    browser_version?: string | undefined;
+    os_name?: string | undefined;
+    os_version?: string | undefined;
+}
+
+export interface RequestResponse {
+    data: {
+        result: RequestResult,
+    };
+}
+
+export interface TestsResult {
+    test_suite_status: string;
+    test_suite_duration: string;
+    rolledup_test_success_count: number;
+    rolledup_test_failure_count: number;
+    rolledup_test_error_count: number;
+    rolledup_test_skip_count: number;
+    child_suite_results: TestsResult[];
+}
+
+export interface RequestResult extends TestsResult {
+    links: {
+        progress: {
+            id: string,
+            url: string,
+        },
+        results: {
+            id: string,
+            url: string,
+        }
+    };
+    status: string;
+    status_label: string;
+    status_message: string;
+    status_detail: string;
+    error: string;
+    percent_complete: number;
+}
+
+export enum ResponseStatus {
+    Pending = 0,
+    Running = 1,
+    Successful = 2,
+    Failed = 3,
+    Canceled = 4,
+}
+
+export interface axiosConfig {
+    headers: {
+        Accept: string,
+    };
+    auth: User;
+}
